6086673c9f22a197cb8fb410006c9222044d76f7
dayubadel <dayana_bailon@outlook.com>
2020-01-14 11:03:05 -0500

    tokenController

---
+++ API/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using API.Models.Catalogos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API.Controllers
+{
+    public class TokenController : Controller
+    {
+        CatalogoTokens CatTokens = new CatalogoTokens();
+        /*
+         *  1	INSERTAR
+            2	MODIFICAR
+            3	ELIMINAR
+            4	CONSULTAR
+         */
+        [HttpGet]
+        [Route("api/tokens")]
+        public object tokens()
+        {
+
+            return CatTokens.GenerarTokens();
+        }
+    }
+}
