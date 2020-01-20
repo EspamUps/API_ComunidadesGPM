b501a0403c8bb8c709646a2c5e2f3c2272a45628
maronv64 <maron.v64@gmail.com>
2020-01-20 08:56:27 -0500

    d

--- API/Controllers/PersonaController.cs
+++ API/Controllers/PersonaController.cs
@@ -23,15 +23,8 @@ namespace API.Controllers
         [HttpPost]
         [Route("api/persona_insertar")]
         public object persona_insertar(Persona _objPersona)
-<<<<<<< HEAD
-        {
-            //return _objPersona;
-            object _respuesta = new object();
-           
-=======
         {            
             object _respuesta = new object();           
->>>>>>> 759bbff37a56980cc5d7425ccaefefecf8821872
             RespuestaHTTP _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "500").FirstOrDefault();
             try
             {
@@ -82,15 +75,10 @@ namespace API.Controllers
                 }
                 else
                 {
-<<<<<<< HEAD
 
                     int _idSexoDesencriptado                = Convert.ToInt32(_seguridad.DesEncriptar(_objPersona.Sexo.IdSexoEncriptado));
                     int _idTipoIdentificacionDesencriptado  = Convert.ToInt32(_seguridad.DesEncriptar(_objPersona.TipoIdentificacion.IdTipoIdentificacionEncriptado));
                     int _idParroquiaDesencriptado           = Convert.ToInt32(_objPersona.Parroquia.IdParroquiaEncriptado); // aun no existe esta tabla por eso paso un valor string y lo convierto;
-=======
-                    int _idSexoDesencriptado = Convert.ToInt32(_seguridad.DesEncriptar(_objPersona.Sexo.IdSexoEncriptado));
-                    int _idTipoIdentificacionDesencriptado = Convert.ToInt32(_seguridad.DesEncriptar(_objPersona.TipoIdentificacion.IdTipoIdentificacionEncriptado));
->>>>>>> 759bbff37a56980cc5d7425ccaefefecf8821872
 
                     var _objSexo = _objCatalogoSexo.ConsultarSexos().Where(c => c.IdSexo == _idSexoDesencriptado && c.Estado == true).FirstOrDefault();
                     var _objTipoIdentificacion = _objCatalogoTipoIdentificacion.ConsultarTipoIdentificacion().Where(c => c.IdTipoIdentificacion == _idTipoIdentificacionDesencriptado && c.Estado == true).FirstOrDefault();
@@ -107,35 +95,12 @@ namespace API.Controllers
                     }
                     else
                     {
-<<<<<<< HEAD
-                        _objPersona.Estado                                  = true;
-                        _objPersona.Sexo.IdSexo                             = _idSexoDesencriptado;
-                        _objPersona.TipoIdentificacion.IdTipoIdentificacion = _idTipoIdentificacionDesencriptado;
-                        _objPersona.Parroquia.IdParroquia                   = _idParroquiaDesencriptado;
-
-                        int _idPersonaIngresado = _objCatalogoPersona.InsertarPersona(_objPersona);
-                        if (_idPersonaIngresado == 0)
-                        {
-                            _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "400").FirstOrDefault();
-                            _http.mensaje = "Ocurrió un error al intentar ingresar al usuario, intente nuevamente.";
-                        }
-                        else
-                        {
-                            var _personaIngresada = _objCatalogoPersona.ConsultarPersona().Where(c => c.IdPersona == _idPersonaIngresado && c.Estado == true).FirstOrDefault();
-                            _personaIngresada.IdPersona = 0;
-                            _personaIngresada.Sexo.IdSexo = 0;
-                            _personaIngresada.TipoIdentificacion.IdTipoIdentificacion = 0;
-                            _respuesta = _personaIngresada;
-                            _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "200").FirstOrDefault();
-                        }
-=======
                         var _personaIngresada = _objCatalogoPersona.ConsultarPersona().Where(c => c.IdPersona == _idPersonaIngresado && c.Estado == true).FirstOrDefault();
                         _personaIngresada.IdPersona = 0;
                         _personaIngresada.Sexo.IdSexo = 0;
                         _personaIngresada.TipoIdentificacion.IdTipoIdentificacion = 0;
                         _respuesta = _personaIngresada;
                         _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "200").FirstOrDefault();
->>>>>>> 759bbff37a56980cc5d7425ccaefefecf8821872
                     }
 
                 }
