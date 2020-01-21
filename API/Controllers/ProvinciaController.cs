6b72541130e19da2f3e7e95256afae8d6fee48f3
dayubadel <dayana_bailon@outlook.com>
2020-01-21 15:01:53 -0500

    consulta individual provincia controller

--- API/Controllers/ProvinciaController.cs
+++ API/Controllers/ProvinciaController.cs
@@ -1,11 +1,13 @@
 ﻿using API.Models.Catalogos;
 using API.Models.Metodos;
+using API.Models.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+
 
 namespace API.Controllers
 {
@@ -23,7 +25,7 @@ namespace API.Controllers
             RespuestaHTTP _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "500").FirstOrDefault();
             try
             {
-                var _listaProvincias = _objCatalogoProvincia.ConsultarProvincia().Where(c=>c.EstadoProvincia==true).ToList();
+                var _listaProvincias = _objCatalogoProvincia.ConsultarProvincia().Where(c => c.EstadoProvincia == true).ToList();
                 foreach (var item in _listaProvincias)
                 {
                     item.IdProvincia = 0;
@@ -33,15 +35,41 @@ namespace API.Controllers
             }
             catch (Exception ex)
             {
-
                 _http.mensaje = _http.mensaje + " " + ex.Message.ToString();
-                return new
-                {
-                    respuesta = _respuesta, http = _http
-                };
-
+                return new {respuesta = _respuesta, http = _http };
             }
             return new { respuesta = _respuesta, http = _http };
+        }
+
+
+
+        [HttpPost]
+        [Route("api/provincia_consultar")]
+        public object provincia_consultar(string _idProvinciaEncriptado)
+        {
+            object _respuesta = new object();
+            RespuestaHTTP _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "500").FirstOrDefault();
+            try
+            {
+                if (string.IsNullOrEmpty(_idProvinciaEncriptado))
+                {
+                    _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "400").FirstOrDefault();
+                }
+                else
+                {
+                    int _idProvincia = Convert.ToInt32(_seguridad.DesEncriptar(_idProvinciaEncriptado));
+                    var _objProvincia = _objCatalogoProvincia.ConsultarProvinciaPorId(_idProvincia).Where(c => c.EstadoProvincia == true).FirstOrDefault();
+                    _objProvincia.IdProvincia = 0;
+                    _respuesta = _objProvincia;
+                    _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "200").FirstOrDefault();
+                }
             }
+            catch (Exception ex)
+            {
+                _http.mensaje = _http.mensaje + " " + ex.Message.ToString();
+                return new { respuesta = _respuesta, http = _http };
+            }
+            return new { respuesta = _respuesta, http = _http };
         }
     }
+}
--- API/Models/Catalogos/CatalogoProvincia.cs
+++ API/Models/Catalogos/CatalogoProvincia.cs
@@ -30,5 +30,55 @@ namespace API.Models.Catalogos
             }
             return _lista;
         }
+
+        public List<Provincia> ConsultarProvinciaPorId(int _idProvincia)
+        {
+            List<Provincia> _lista = new List<Provincia>();
+            foreach (var item in db.Sp_ProvinciaConsultar().Where(c => c.IdProvincia == _idProvincia).ToList())
+            {
+                _lista.Add(new Provincia()
+                {
+                    IdProvincia = item.IdProvincia,
+                    IdProvinciaEncriptado = _seguridad.Encriptar(item.IdProvincia.ToString()),
+                    CodigoProvincia = item.CodigoProvincia,
+                    DescripcionProvincia = item.DescripcionProvincia,
+                    NombreProvincia = item.NombreProvincia,
+                    RutaLogoProvincia = item.RutaLogoProvincia,
+                    EstadoProvincia = item.EstadoProvincia
+                });
+            }
+            return _lista;
+        }
+
+        public int ModificarProvincia(Provincia _objProvincia)
+        {
+            try
+            {
+                db.Sp_ProvinciaModificar(_objProvincia.IdProvincia, _objProvincia.CodigoProvincia, _objProvincia.NombreProvincia, _objProvincia.DescripcionProvincia, _objProvincia.RutaLogoProvincia, _objProvincia.EstadoProvincia);
+                return _objProvincia.IdProvincia;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int InsertarProvincia(Provincia _objProvincia)
+        {
+            try
+            {
+                return int.Parse(db.Sp_ProvinciaInsertar(_objProvincia.CodigoProvincia, _objProvincia.NombreProvincia, _objProvincia.DescripcionProvincia, _objProvincia.RutaLogoProvincia, _objProvincia.EstadoProvincia).Select(x=>x.Value.ToString()).FirstOrDefault());
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int EliminarProvincia(int _idProvincia)
+        {
+            db.Sp_ProvinciaEliminar(_idProvincia);
+            return _idProvincia;
+        }
     }
 }
