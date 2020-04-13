2bcdbcb3d6742903f5c82cdfae65f6c674e9605f
dayubadel <dayana_bailon@outlook.com>
2020-04-12 19:28:46 -0500

    lista de preguntas con identificador 2 y 3 con total de respuestas

--- API/Controllers/VersionamientoPreguntaController.cs
+++ API/Controllers/VersionamientoPreguntaController.cs
@@ -16,6 +16,12 @@ namespace API.Controllers
         CatalogoVersionamientoPregunta _objCatalogoVersionamientoPregunta = new CatalogoVersionamientoPregunta();
         CatalogoCabeceraVersionCuestionario _objCatalogoCabeceraVersionCuestionario = new CatalogoCabeceraVersionCuestionario();
         CatalogoAsignarComponenteGenerico _objCatalogoAsignarComponenteGenerico = new CatalogoAsignarComponenteGenerico();
+        CatalogoAsignarResponsableModeloPublicado _objCatalogoAsignarResponsableModeloPublicado = new CatalogoAsignarResponsableModeloPublicado();
+        CatalogoComunidad _objCatalogoComunidad = new CatalogoComunidad();
+        CatalogoRespuesta _objCatalogoRespuesta = new CatalogoRespuesta();
+        CatalogoAsignarEncuestado _objCatalogoAsignarEncuestado = new CatalogoAsignarEncuestado();
+        CatalogoCabeceraRespuesta _objCatalogoCabeceraRespuesta = new CatalogoCabeceraRespuesta();
+        CatalogoOpcionPreguntaSeleccion _objCatalogoOpcionPreguntaSeleccion = new CatalogoOpcionPreguntaSeleccion();
         Seguridad _seguridad = new Seguridad();
 
         [HttpPost]
@@ -73,17 +79,21 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        [Route("api/versionamientopregunta_consultarporidasignarcomponentegenerico")]
-        public object versionamientopregunta_consultarporidasignarcomponentegenerico(string _idAsignarComponenteGenericoEncriptado)
+        [Route("api/versionamientopregunta_consultarporidasignarcomponentegenericoporidasignarresponsablemodelpublicado")]
+        public object versionamientopregunta_consultarporidasignarcomponentegenericoporidasignarresponsablemodelpublicado(string _idAsignarComponenteGenericoEncriptado, string _idAsignarResponsableModeloPublicadoEncriptado)
         {
             object _respuesta = new object();
             RespuestaHTTP _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "500").FirstOrDefault();
             try
             {
-                if (_idAsignarComponenteGenericoEncriptado == null ||string.IsNullOrEmpty(_idAsignarComponenteGenericoEncriptado))
+                if (_idAsignarComponenteGenericoEncriptado == null || string.IsNullOrEmpty(_idAsignarComponenteGenericoEncriptado))
                 {
                     _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "400").FirstOrDefault();
-                    _http.mensaje = "Ingrese el identificador del asignar componente generico";
+                    _http.mensaje = "Ingrese el identificador del asignar componente genérico";
+                } else if (_idAsignarResponsableModeloPublicadoEncriptado == null || string.IsNullOrEmpty(_idAsignarResponsableModeloPublicadoEncriptado))
+                {
+                    _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "400").FirstOrDefault();
+                    _http.mensaje = "Ingrese el identificador del asignar responsable modelo publicado";
                 }
                 else
                 {
@@ -96,19 +106,69 @@ namespace API.Controllers
                     }
                     else
                     {
-
-                        var _listaVersionamiento = _objCatalogoVersionamientoPregunta.ConsultarVersionamientoPreguntaCompletoPorIdCabeceraVersionPorIdComponente(_objAsignarComponenteGenerico.AsignarCuestionarioModelo.CuestionarioPublicado.CabeceraVersionCuestionario.IdCabeceraVersionCuestionario,_objAsignarComponenteGenerico.Componente.IdComponente).ToList();
-                        foreach (var _objVersionamiento in _listaVersionamiento)
+                        int _idAsignarResponsableModeloPublicado = Convert.ToInt32(_seguridad.DesEncriptar(_idAsignarResponsableModeloPublicadoEncriptado));
+                        var _objAsignarResponsableModeloPublicado = _objCatalogoAsignarResponsableModeloPublicado.ConsultarAsignarResponsableModeloPublicadoPorId(_idAsignarResponsableModeloPublicado).FirstOrDefault();
+                        if (_objAsignarResponsableModeloPublicado == null)
                         {
-                            _objVersionamiento.IdVersionamientoPregunta = 0;
-                            _objVersionamiento.CabeceraVersionCuestionario.IdCabeceraVersionCuestionario = 0;
-                            _objVersionamiento.Pregunta.IdPregunta = 0;
-                            _objVersionamiento.Pregunta.TipoPregunta.IdTipoPregunta = 0;
-                            _objVersionamiento.Pregunta.Seccion.IdSeccion = 0;
-                            _objVersionamiento.Pregunta.Seccion.Componente.IdComponente = 0;
+                            _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "404").FirstOrDefault();
+                            _http.mensaje = "No se encontró el objeto asignar responsable del modelo publicado";
                         }
-                        _respuesta = _listaVersionamiento;
-                        _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "200").FirstOrDefault();
+                        else
+                        {
+                            var _listaComunidades = _objCatalogoComunidad.ConsultarComunidadPorIdParroquia(_objAsignarResponsableModeloPublicado.Parroquia.IdParroquia).Where(c => c.EstadoComunidad == true).ToList();
+                            int _idCuestionarioPublicado = _objAsignarComponenteGenerico.AsignarCuestionarioModelo.CuestionarioPublicado.IdCuestionarioPublicado;
+                            var _listaAsignarEncuestadoPorCuestionarioPublicado = _objCatalogoAsignarEncuestado.ConsultarAsignarEncuestadPorIdCuestionarioPublicado(_idCuestionarioPublicado).Where(c => c.Estado == true).ToList();
+                            List<Respuesta> _listaRespuestasTodosCuestionariosComunidades = new List<Respuesta>();
+                            foreach (var itemComunidad in _listaComunidades)
+                            {
+                                var _objAsignarEncuestadoPorComunidad = _listaAsignarEncuestadoPorCuestionarioPublicado.Where(c => c.Comunidad.IdComunidad == itemComunidad.IdComunidad).FirstOrDefault();
+                                if(_objAsignarEncuestadoPorComunidad!=null)
+                                {
+                                    var _objCabeceraRespuesta = _objCatalogoCabeceraRespuesta.ConsultarCabeceraRespuestaPorIdAsignarEncuestado(_objAsignarEncuestadoPorComunidad.IdAsignarEncuestado).Where(c=>c.Finalizado==true && c.Estado==true).FirstOrDefault();
+                                    if(_objCabeceraRespuesta!=null)
+                                    {
+                                        var _listaRespuestasPorCabeceraPorComunidad = _objCatalogoRespuesta.ConsultarRespuestaPorIdCabeceraRespuesta(_objCabeceraRespuesta.IdCabeceraRespuesta).ToList();
+                                        _listaRespuestasTodosCuestionariosComunidades.AddRange(_listaRespuestasPorCabeceraPorComunidad);
+                                    }
+                                }
+                            }
+                            var _listaOpcionPreguntaSeleccion = _objCatalogoOpcionPreguntaSeleccion.ConsultarOpcionPreguntaSeleccion().Where(c => c.Estado == true).ToList();
+                            var _listaVersionamiento = _objCatalogoVersionamientoPregunta.ConsultarVersionamientoPreguntaCompletoPorIdCabeceraVersionPorIdComponente(_objAsignarComponenteGenerico.AsignarCuestionarioModelo.CuestionarioPublicado.CabeceraVersionCuestionario.IdCabeceraVersionCuestionario, _objAsignarComponenteGenerico.Componente.IdComponente).ToList();
+                            _listaVersionamiento = _listaVersionamiento.Where(c => c.Pregunta.TipoPregunta.Identificador != 1).ToList();
+                            foreach (var itemVersionamiento in _listaVersionamiento)
+                            {
+                                List<RespuestasCaracterizacion> _listaRespuestasCaracterizacion = new List<RespuestasCaracterizacion>();
+                                if(itemVersionamiento.Pregunta.TipoPregunta.Identificador==2 || itemVersionamiento.Pregunta.TipoPregunta.Identificador==3)
+                                {
+                                    var _listasRespuestasPorPregunta = _listaRespuestasTodosCuestionariosComunidades.Where(c => c.Pregunta.IdPregunta == itemVersionamiento.Pregunta.IdPregunta).ToList();
+                                    var _listaOpcionPreguntaSeleccionPorPregunta = _listaOpcionPreguntaSeleccion.Where(c => c.Pregunta.IdPregunta == itemVersionamiento.Pregunta.IdPregunta).ToList();
+                                    foreach (var itemOpcionPreguntaSeleccion in _listaOpcionPreguntaSeleccionPorPregunta)
+                                    {
+                                        var _totalRespuestasPorOpcion = _listasRespuestasPorPregunta.Where(c => c.IdRespuestaLogica == itemOpcionPreguntaSeleccion.IdOpcionPreguntaSeleccion).ToList().Count;
+                                        _listaRespuestasCaracterizacion.Add(new RespuestasCaracterizacion()
+                                        {
+                                            IdRespuestasCaracterizacion = itemOpcionPreguntaSeleccion.IdOpcionPreguntaSeleccion,
+                                            IdRespuestasCaracterizacionEncriptado = _seguridad.Encriptar(itemOpcionPreguntaSeleccion.IdOpcionPreguntaSeleccion.ToString()),
+                                            Descripcion =itemOpcionPreguntaSeleccion.Descripcion,
+                                            Total=_totalRespuestasPorOpcion
+                                        });
+                                    }
+                                    itemVersionamiento.Pregunta.ListaRespuestasCaracterizacion = _listaRespuestasCaracterizacion;
+                                }
+                            }
+
+                            foreach (var _objVersionamiento in _listaVersionamiento)
+                            {
+                                _objVersionamiento.IdVersionamientoPregunta = 0;
+                                _objVersionamiento.CabeceraVersionCuestionario.IdCabeceraVersionCuestionario = 0;
+                                _objVersionamiento.Pregunta.IdPregunta = 0;
+                                _objVersionamiento.Pregunta.TipoPregunta.IdTipoPregunta = 0;
+                                _objVersionamiento.Pregunta.Seccion.IdSeccion = 0;
+                                _objVersionamiento.Pregunta.Seccion.Componente.IdComponente = 0;
+                            }
+                            _respuesta = _listaVersionamiento;
+                            _http = _objCatalogoRespuestasHTTP.consultar().Where(x => x.codigo == "200").FirstOrDefault();
+                        }
                     }
                 }
             }
